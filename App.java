0dc7f4faabeb2035f6ca6b12343e68b0c454c946
Rodrigo Araújo <fenrrir@gmail.com>
2013-11-12 17:17:44 -0300

    codigo de teste

---
+++ App.java
@@ -0,0 +1,15 @@
+
+public class App {
+	
+	public static void main(String[] args) {
+		Locadora loc = new Locadora();
+		loc.addVeiculos( new Carro("id1", 12000.00, 0.8, "mx", 16.0) );
+		loc.addVeiculos( new Carro("id2", 120.00, 0.8, "mx", 16.0) );
+		loc.addVeiculos( new Aviao("id3", 10000000.00, 10, 800) );
+		System.out.println(loc.getDiaria("id3"));
+		System.out.println(loc.quantidadeVeiculos());
+		System.out.println(loc.getVeiculoDiariaMaisCara().getId());
+		System.out.println(loc.getVeiculosPorTipo(Aviao.class));
+	}
+
+}
